2a8b100c0d1121500a37075ec8f3a462d495f250
JorgeChaparroS <jorgechaparrosarmiento@gmail.com>
2023-11-21 20:12:17 -0500

    Pages, routing, author page, footer, header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { AuthorComponent } from './pages/author/author.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'projects',
+        pathMatch: 'full'
+    }, 
+    {
+        path: 'projects',
+        title: 'JorgeChaparro`S Portfolio - Projects',
+        loadComponent: () => import('./pages/projects/projects.component').then(component => component.ProjectsComponent)
+    }, 
+    {
+        path: 'about', 
+        title: 'JorgeChaparro`S Portfolio - About',
+        loadComponent: () => import('./pages/about/about.component').then(component => component.AboutComponent)
+    },
+    {
+        path: 'author',
+        title: 'JorgeChaparro`S Portfolio - Author',
+        loadComponent: () => import('./pages/author/author.component').then(component => component.AuthorComponent)
+    }
+];
---
+++ src/app/pages/author/author.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChapsAngularLibModule } from 'chaps-angular-lib';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-author',
+  standalone: true,
+  imports: [CommonModule, ChapsAngularLibModule, RouterLink],
+  templateUrl: './author.component.html',
+  styleUrl: './author.component.scss'
+})
+export class AuthorComponent {
+
+}
