f24ff5ab71db88c63c054ced4f447945bf721e44
JorgeChaparroS <jorgechaparrosarmiento@gmail.com>
2023-11-26 12:15:53 -0500

    Projects and filtering by form options

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -4,6 +4,7 @@ import { ChapsAngularLibModule, DropdownOption } from 'chaps-angular-lib';
 import { FormsModule } from '@angular/forms';
 import { Constants } from '../../utils/Contants';
 import { TechSelectorComponent } from '../../components/tech-selector/tech-selector.component';
+import { Project } from '../../model';
 
 @Component({
   selector: 'app-projects',
@@ -14,8 +15,8 @@ import { TechSelectorComponent } from '../../components/tech-selector/tech-selec
 })
 export class ProjectsComponent {
 
-  technologySelected = null;
-  technologyOptions: DropdownOption[] = [
+  technologyApproachSelected = null;
+  technologyApproachOptions: DropdownOption[] = [
     { value: Constants.TECHNOLOGY_OPTIONS.FRONTEND, name: 'Front-end' },
     { value: Constants.TECHNOLOGY_OPTIONS.BACKEND, name: 'Back-end' },
     { value: Constants.TECHNOLOGY_OPTIONS.FULLSTACK, name: 'Full-stack' },
@@ -31,16 +32,53 @@ export class ProjectsComponent {
 
   availableTechnologies: any[] = Constants.TECHNOLOGIES;
 
+  projects: Project[] = Constants.PROJECTS;
+
   onSearch(): void {
-    console.log('onSearch', this.technologySelected, this.contributionSelected, this.availableTechnologies);
+    const technologiesSelected = this.availableTechnologies.filter((technology: any) => technology.selected);
+    const stackSelected = technologiesSelected.map((technology: any) => technology.value);
+    this.projects = Constants.PROJECTS
+        .filter((project: Project) => this.matchesProjectType(project))
+        .filter((project: Project) => this.matchesContributionType(project))
+        .filter((project: Project) => this.implementsSelectedTechStack(project, stackSelected));
   }
+
+  matchesProjectType(project: Project): boolean {
+    if (!this.technologyApproachSelected || this.technologyApproachSelected === Constants.TECHNOLOGY_OPTIONS.ALL) {
+      return true;
+    } else {
+      return project.approach === this.technologyApproachSelected;
+    }
+  }
+
+  matchesContributionType(project: Project): boolean {
+    if (!this.contributionSelected || this.contributionSelected === Constants.CONTRIBUTION_OPTIONS.ALL) {
+      return true;
+    } else {
+      return project.contributionType === this.contributionSelected;
+    }
+  }
+
+  implementsSelectedTechStack(project: Project, projectStack: string[]): boolean {
+    if (projectStack.length < 1) {
+      return true;
+    } else {
+      for (let techStack of projectStack) {
+        const techAppliedInProject = project.technologies.find((techInProject: string) => techStack === techInProject);
+        if (!techAppliedInProject) {
+          return false;
+        }
+      }
+      return true;
+    }
+  };
 
   onTechSelected(index: number): void {
     this.availableTechnologies[index].selected = !(this.availableTechnologies[index].selected || false);
   }
 
   onClearSearch(): void {
-    this.technologySelected = null;
+    this.technologyApproachSelected = null;
     this.contributionSelected = null;
     this.availableTechnologies.forEach((technology: any) => technology.selected = false);
     this.onSearch();
--- src/app/utils/Contants.ts
+++ src/app/utils/Contants.ts
@@ -1,3 +1,5 @@
+import { Project } from "../model";
+
 export class Constants {
 
     static readonly TECHNOLOGY_OPTIONS = {
@@ -41,5 +43,103 @@ export class Constants {
         Constants.TECHNOLOGY_TYPE.UNITTESTING,
         Constants.TECHNOLOGY_TYPE.DEPENDENCY,
         Constants.TECHNOLOGY_TYPE.APPLICATION
+    ];
+
+    static readonly PROJECTS: Project[] = [
+        {
+            title: 'Naruto Wiki (Angular)',
+            image: '../../../assets/images/naruto-wiki-angular.png',
+            approach: Constants.TECHNOLOGY_OPTIONS.FRONTEND,
+            technologies: [
+                Constants.TECHNOLOGY_TYPE.ANGULAR.value,
+                Constants.TECHNOLOGY_TYPE.BOOTSTRAP.value,
+                Constants.TECHNOLOGY_TYPE.HTML.value,
+                Constants.TECHNOLOGY_TYPE.CSS.value,
+                Constants.TECHNOLOGY_TYPE.SCSS.value,
+                Constants.TECHNOLOGY_TYPE.TYPESCRIPT.value,
+                Constants.TECHNOLOGY_TYPE.UNITTESTING.value,
+                Constants.TECHNOLOGY_TYPE.APPLICATION.value,
+            ],
+            description: [
+                'This is an Angular project that uses some of the main tools for this framework, such as: components, services, routing, modules, lazy loading, reactive forms, API calls, Http interceptors.',
+                'Other tools for front end development were use, such as: Bootstrap, CSS preprocessors, internationalization, unit testing.'
+            ],
+            contributionType: Constants.CONTRIBUTION_OPTIONS.OWN,
+            appLink: 'https://naruto-wiki-jorgechaparros-dev.netlify.app',
+            repository: [
+                { label: 'Github repository', link:'https://github.com/JorgeChaparroS/naruto-angular' }
+            ]
+        },
+        {
+            title: 'Naruto Wiki (React)',
+            image: '../../../assets/images/naruto-wiki-react.png',
+            approach: Constants.TECHNOLOGY_OPTIONS.FRONTEND,
+            technologies: [
+                Constants.TECHNOLOGY_TYPE.REACT.value,
+                Constants.TECHNOLOGY_TYPE.BOOTSTRAP.value,
+                Constants.TECHNOLOGY_TYPE.HTML.value,
+                Constants.TECHNOLOGY_TYPE.CSS.value,
+                Constants.TECHNOLOGY_TYPE.SCSS.value,
+                Constants.TECHNOLOGY_TYPE.JAVASCRIPT.value,
+                Constants.TECHNOLOGY_TYPE.NEXTJS.value,
+                Constants.TECHNOLOGY_TYPE.APPLICATION.value,
+            ],
+            description: [
+                'This is a NextJS (React Framework) project that uses some of the main tools for this framework, such as: components, routing, API calls.',
+                'Other tools for front end development were use, such as: Bootstrap, CSS preprocessors, internationalization'
+            ],
+            contributionType: Constants.CONTRIBUTION_OPTIONS.OWN,
+            appLink: 'https://naruto-wiki-jorgechaparros-dev-react.netlify.app/',
+            repository: [
+                { label: 'Github repository', link:'https://github.com/JorgeChaparroS/naruto-react' }
+            ]
+        },
+        {
+            title: 'Chaps-Angular-lib',
+            image: '../../../assets/images/chaps-angular-lib.png',
+            approach: Constants.TECHNOLOGY_OPTIONS.FRONTEND,
+            technologies: [
+                Constants.TECHNOLOGY_TYPE.ANGULAR.value,
+                Constants.TECHNOLOGY_TYPE.BOOTSTRAP.value,
+                Constants.TECHNOLOGY_TYPE.HTML.value,
+                Constants.TECHNOLOGY_TYPE.CSS.value,
+                Constants.TECHNOLOGY_TYPE.SCSS.value,
+                Constants.TECHNOLOGY_TYPE.TYPESCRIPT.value,
+                Constants.TECHNOLOGY_TYPE.DEPENDENCY.value,
+            ],
+            description: [
+                'This is an Angular Library whose purpose is to store reusable Angular components, so they can be use in every Angular project the dependency is defined in, without the need of defining them again.'
+            ],
+            contributionType: Constants.CONTRIBUTION_OPTIONS.OWN,
+            appLink: 'https://chaps-angular-lib.netlify.app/',
+            repository: [
+                { label: 'Github repository', link:'https://github.com/JorgeChaparroS/naruto-react' },
+                { label: 'Package in NPM', link:'https://www.npmjs.com/package/chaps-angular-lib' },
+                { label: 'Implementing the library in other projects', link:'https://github.com/JorgeChaparroS/chaps-angular-library-implementation' }
+            ]
+        },
+        {
+            title: 'One Piece Desk',
+            image: '../../../assets/images/one-piece-desk.png',
+            approach: Constants.TECHNOLOGY_OPTIONS.FRONTEND,
+            technologies: [
+                Constants.TECHNOLOGY_TYPE.ANGULAR.value,
+                Constants.TECHNOLOGY_TYPE.HTML.value,
+                Constants.TECHNOLOGY_TYPE.CSS.value,
+                Constants.TECHNOLOGY_TYPE.SCSS.value,
+                Constants.TECHNOLOGY_TYPE.TYPESCRIPT.value,
+                Constants.TECHNOLOGY_TYPE.APPLICATION.value,
+            ],
+            description: [
+                'This is an Angular Application that shows relevant information about "One Piece" characters and ships.',
+                'The authorship of this project belongs mostly to Christopher Ortiz (@christophermontero), my contribution was the refactoring of the code with Lazy Loading to improve the performance of the application.'
+            ],
+            contributionType: Constants.CONTRIBUTION_OPTIONS.TEAM,
+            appLink: 'https://onepiecedesk.netlify.app/pirates',
+            repository: [
+                { label: 'Github repository', link:'https://github.com/christophermontero/onepiecedesk' },
+                { label: 'Code refactoring by me', link:'https://github.com/JorgeChaparroS/onepiecedesk' }
+            ]
+        }
     ];
 };
